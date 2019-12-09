0ce04750e579d32fc850c0d185b41097b45bbaff
bkiser <bkiser@republicwireless.com>
2019-12-09 16:27:00 -0500

    exercise 009-010

---
+++ Week 7-12/Exercise-010/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // *******************************
+        // CivilService test = new CivilService();
+        // System.out.println(test.getDaysLeft());
+        // test.work();
+        // System.out.println(test.getDaysLeft());
+
+        // MilitaryService test2 = new MilitaryService(300);
+        // System.out.println(test2.getDaysLeft());
+        // test2.work();
+        // System.out.println(test2.getDaysLeft());
+        // *******************************
+    }
+}
